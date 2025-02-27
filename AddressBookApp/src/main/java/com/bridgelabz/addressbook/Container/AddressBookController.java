b403ddf22bc4f29426088e596bff4bf7bebd1dfe
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-27 13:06:02 +0530

    [Shivam Kumar Jha] Add . Various HTTP method added.

--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/Container/AddressBookController.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/Container/AddressBookController.java
@@ -1,8 +1,15 @@
 package com.bridgelabz.addressbook.Container;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +25,45 @@ public class AddressBookController {
 	
 	@PostMapping("/add")
 	public ResponseEntity<Contact> getContactSaved(@RequestBody Contact contact) {
-	    System.out.println("Received Contact: " + contact);
+	    System.out.println("Received Contact:");
+	    System.out.println("First Name: " + contact.getFirstName());
+	    System.out.println("Last Name: " + contact.getLastName());
+	    System.out.println("Address: " + contact.getAddress());
+	    System.out.println("Email: " + contact.getEmail());
+	    System.out.println("Notes: " + contact.getNotes());
+	    
 	    Contact savedContact = addressBookAppService.saveContact(contact);
 	    return ResponseEntity.ok(savedContact);
 	}
-
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Contact>> getAllContacts() {
+		List<Contact> contacts = addressBookAppService.getAllContacts();
+		return ResponseEntity.ok(contacts);
+	}
+	
+	@GetMapping("/get/{id}")
+	public ResponseEntity<Contact> getById(@PathVariable int id) {
+		try {
+			Contact contact = addressBookAppService.getById(id);
+			return ResponseEntity.ok(contact);
+		} catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Contact> updateById(@PathVariable int id, @RequestBody Contact contact) {
+		Contact updatedContact = addressBookAppService.updateById(id, contact);
+		return ResponseEntity.ok(updatedContact);
+	}
+	
+	@DeleteMapping("/delete/{id}") 
+	public ResponseEntity<String> deleteById(@PathVariable int id) {
+		try {
+			return ResponseEntity.ok(addressBookAppService.deleteById(id));
+		} catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
 }
--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/Service/AddressBookAppService.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/Service/AddressBookAppService.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.addressbook.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,34 @@ public class AddressBookAppService {
 	
 	public Contact saveContact(Contact contact) {
 		return addressRepository.save(contact);
+	}
+
+	public List<Contact> getAllContacts() {
+		return addressRepository.findAll();
+	}
+
+	public Contact getById(int id) {
+		return addressRepository.findById(id).orElseThrow(() -> new RuntimeException("Contact id not found!"));
+	}
+
+	public Contact updateById(int id, Contact contact) {
+		return addressRepository.findById(id).map(existingContact -> {
+			existingContact.setFirstName(contact.getFirstName());
+			existingContact.setLastName(contact.getLastName());
+			existingContact.setEmail(contact.getEmail());
+			existingContact.setAddress(contact.getAddress());
+			existingContact.setNotes(contact.getNotes());
+			
+			return addressRepository.save(existingContact);
+		}).orElseThrow(() -> new RuntimeException("Given contact id not found!"));
+	}
+
+	public String deleteById(int id) {
+		if(addressRepository.existsById(id)) {
+			addressRepository.deleteById(id);
+			return "Contact deleted successfully!";
+		} 
+		
+		return "Contact with given id doesn't exists.";
 	}
 }
